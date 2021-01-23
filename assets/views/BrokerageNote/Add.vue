0ee02e30a97186e0bf7428f1986e24213cad99fb
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-01-22 21:34:07 -0300

    Add route to add brokerage notes

---
+++ assets/views/BrokerageNote/Add.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Nova nota de corretagem</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "BrokerageNoteAdd"
+  }
+</script>
+
+<style scoped>
+
+</style>
