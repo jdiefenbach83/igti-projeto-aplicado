128cf8ad2b42957d8f106ecbacfbe8df1e800bf3
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-02-17 23:27:02 -0300

    Change de route processing to allow refresh to frontend

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,12 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class IndexController extends AbstractController
 {
-    public function index(): Response
+    const ALLOWED_ROUTES_PREFIX = [
+        '/',
+        'brokers',
+        'brokerageNotes',
+    ];
+
+    public function index(Request $request): Response
     {
-        return $this->render('base.html.twig', []);
+        $pathInfo = $request->getPathInfo();
+
+        if ($this->shouldRenderFrontEnd($pathInfo)){
+            return $this->render('base.html.twig', []);
+        }
+
+        throw $this->createNotFoundException();
+    }
+
+    private function shouldRenderFrontEnd(string $pathInfo): bool
+    {
+        foreach (self::ALLOWED_ROUTES_PREFIX as $route) {
+            if (str_contains($pathInfo, $route)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
