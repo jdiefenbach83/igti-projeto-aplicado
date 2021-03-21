171e788d074422d95c2a7b4c31e1e1bd918789a1
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-03-21 17:03:20 -0300

    Add Position entity

---
+++ migrations/Version20210321195550.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210321195550 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Create position table';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE position (id INT AUTO_INCREMENT NOT NULL, operation_id INT NULL, asset_id INT NOT NULL, sequence INT NOT NULL, type ENUM(\'BUY\', \'SELL\'), date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', quantity INT NOT NULL, unit_cost NUMERIC(14, 4) NOT NULL, accumulated_quantity INT NOT NULL, total_operation NUMERIC(14, 4) NOT NULL, accumulater_total INT NOT NULL, average_price NUMERIC(14, 4) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX IDX_OPERATION_OPERATION_ID (operation_id), INDEX IDX_OPERATION_ASSET_ID (asset_id), UNIQUE INDEX UK_OPERATION_1 (asset_id, sequence), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE position ADD CONSTRAINT FK_POSITION_OPERATION_ID FOREIGN KEY (operation_id) REFERENCES operation (id)');
+        $this->addSql('ALTER TABLE position ADD CONSTRAINT PK_POSITION_ASSET_ID FOREIGN KEY (asset_id) REFERENCES asset (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE position');
+    }
+}
---
+++ tests/Unit/Entity/PositionTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\Asset;
+use App\Entity\Company;
+use App\Entity\Position;
+use Faker\Factory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+
+class PositionTest extends TestCase
+{
+    private Generator $faker;
+
+    public function setUp(): void
+    {
+        $this->faker = Factory::create();
+    }
+
+    private function buildAsset(): Asset
+    {
+        $cnpj = $this->faker->text(18);
+        $name = $this->faker->text(255);
+
+        $company = new Company();
+        $company
+            ->setCnpj($cnpj)
+            ->setName($name);
+
+        $types = [
+            Asset::TYPE_STOCK,
+            Asset::TYPE_FUTURE_CONTRACT
+        ];
+
+        $code = $this->faker->text(10);
+        $type = $this->faker->randomElement($types);
+
+        $asset = new Asset();
+        $asset
+            ->setCode($code)
+            ->setType($type)
+            ->setCompany($company);
+
+        return $asset;
+    }
+
+    public function testAsset_ShouldSetAndGetSuccessfully()
+    {
+       $asset = $this->buildAsset();
+       $sequence = $this->faker->numberBetween(1, 100);
+       $type = $this->faker->randomElement(Position::getTypes());
+       $date = \DateTimeImmutable::createFromMutable($this->faker->dateTime());
+       $quantity = $this->faker->numberBetween(1, 100);
+       $unitCost = $this->faker->randomFloat(2, 1, 500);
+       $accumulatedQuantity = $this->faker->numberBetween(1, 1_000);
+       $totalOperation = $this->faker->randomFloat(2, 1, 50_000);
+       $accumulatedTotal = $this->faker->numberBetween(1, 10_000);
+       $averagePrice = $this->faker->randomFloat(2, 1, 500);
+
+       $position = new Position();
+       $position
+           ->setAsset($asset)
+           ->setSequence($sequence)
+           ->setType($type)
+           ->setDate($date)
+           ->setQuantity($quantity)
+           ->setUnitCost($unitCost)
+           ->setAccumulatedQuantity($accumulatedQuantity)
+           ->setTotalOperation($totalOperation)
+           ->setAccumulatedTotal($accumulatedTotal)
+           ->setAveragePrice($averagePrice);
+
+       self::assertEquals($asset, $position->getAsset());
+       self::assertEquals($sequence, $position->getSequence());
+       self::assertEquals($type, $position->getType());
+       self::assertEquals($date, $position->getDate());
+       self::assertEquals($quantity, $position->getQuantity());
+       self::assertEquals($unitCost, $position->getUnitCost());
+       self::assertEquals($accumulatedQuantity, $position->getAccumulatedQuantity());
+       self::assertEquals($totalOperation, $position->getTotalOperation());
+       self::assertEquals($accumulatedTotal, $position->getAccumulatedTotal());
+       self::assertEquals($averagePrice, $position->getAveragePrice());
+       self::assertNull($position->getOperation());
+    }
+}
