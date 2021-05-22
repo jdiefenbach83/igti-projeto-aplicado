22cae7b774bf31606e1aaa922cff76ea9646b6ac
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-05-22 18:52:10 -0300

    Add Unit of Work interface to GoodRepositoryInterface

--- src/Repository/GoodRepositoryInterface.php
+++ src/Repository/GoodRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Good;
 
-interface GoodRepositoryInterface
+interface GoodRepositoryInterface extends WorkUnitInterface
 {
     public function findAll(): array;
     public function save(Good $good): void;
