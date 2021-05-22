a023e7f5d720ca29680c19b2f0a0c3d01fde3228
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-05-22 17:47:05 -0300

    Add variable type to Asset service test

--- tests/Functional/Service/AssetServiceTest.php
+++ tests/Functional/Service/AssetServiceTest.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class AssetServiceTest extends KernelTestCase
 {
-    private $assetService;
+    private ?AssetService $assetService;
 
     private Generator $faker;
 
