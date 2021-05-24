34bb78e3420befba5881095d808ac0669e23ca1e
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-05-24 19:35:06 -0300

    Move test controller classes to correct folders

---
+++ tests/Functional/Controller/ConsolidationControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Functional\Controller;
+
+use App\Tests\Functional\BaseTest;
+
+class ConsolidationControllerTest extends BaseTest
+{
+    public function testConsolidation_shouldGetAllConsolidations(): void
+    {
+        $this->client->request('GET', '/api/consolidations');
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        self::assertNotEmpty($this->client->getResponse()->getContent());
+    }
+}
---
+++ tests/Functional/Controller/GoodControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Functional\Controller;
+
+use App\Tests\Functional\BaseTest;
+
+class GoodControllerTest extends BaseTest
+{
+    public function testGood_shouldGetAllGoods(): void
+    {
+        $this->client->request('GET', '/api/goods');
+
+        self::assertEquals(200, $this->client->getResponse()->getStatusCode());
+        self::assertNotEmpty($this->client->getResponse()->getContent());
+    }
+}
