08c38f1fa5a5804993a7973efaa29c901ca355e2
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-06-06 15:57:09 -0300

    rename LoginController to SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class LoginController
+class SecurityController
 {
     private UserService $service;
     private DTOFactoryInterface $DTOFactory;
